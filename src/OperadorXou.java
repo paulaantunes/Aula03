c7597d7a62bece7cbc031826128f06bd2c1ea828
Paula Antunes <paula-antunes@outlook.com>
2021-07-30 21:05:18 -0300

    Implementado o operador OU exclusivo

--- src/OperadorXou.java
+++ src/OperadorXou.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 public class OperadorXou {
 
 	public static void main(String[] args) {
-		/*Em uma ONG há uma horta monitorada e automatizada por um arduino.
-		 * Escreva o código de um programa que receba a umidade relativa do ar e a temperatura atual. Caso a umidade seja maior que 70% ou caso o temperatura seja maior que 26°C, exibir a mensagem "ligando os ventiladores". Caso as duas condições sejam verdadeiras ou falsas, exibir a mensagem: "Entrar em contato com agrônomo".*/
+		/*Em uma ONG hï¿½ uma horta monitorada e automatizada por um arduino.
+		 * Escreva o cï¿½digo de um programa que receba a umidade relativa do ar e a temperatura atual. Caso a umidade seja maior que 70% ou caso o temperatura seja maior que 26ï¿½C, exibir a mensagem "ligando os ventiladores". Caso as duas condiï¿½ï¿½es sejam verdadeiras ou falsas, exibir a mensagem: "Entrar em contato com agrï¿½nomo".*/
 		Scanner leitor =  new Scanner(System.in);
-		Double umidade, temperatura;
+		double umidade, temperatura;
 		System.out.println("Por favor, insira a umidade relativa do ar:");
 		umidade = leitor.nextDouble();
 		System.out.println("Por favor, insira a temperatura em graus celsius: ");
 		temperatura = leitor.nextDouble();
 		
-		//Aqui é preciso criar um if que verifique as duas condições para que o os ventiladores sejam ligados ou não
+		//Aqui Ã©  preciso criar um if que verifique as duas condicoes para que o os ventiladores sejam ligados ou nï¿½o
+		
+		if (umidade>70 ^ temperatura>26) {
+			System.out.println("ligando os ventiladores");
+		}else {
+			System.out.println("Entrar em contato com agronomo");
+		}
 		
 		leitor.close();
 		
